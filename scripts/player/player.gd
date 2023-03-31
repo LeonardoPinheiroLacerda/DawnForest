8a149bd5a5cf092c3e9f2d10ceb8fde3d3a76de6
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2023-03-31 18:38:13 -0300

    Aula 41

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -20,14 +20,17 @@ func _physics_process(delta: float):
 	vertical_movement_env()
 	gravity(delta)
 	
-	velocity = move_and_slide(velocity)
+	# Primeiro argumento é o vetor de movimentação do personagem
+	# o segundo argumento é a direção de cima para qual o método vai ser referenciar
+	# (importante para o método is_on_floor funcitonar)
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	# Dentro do script atrelado ao nó Texture do player existe esse método para
 	# calcular qual animação o personagem deve executar
 	player_sprite.animate(velocity)
 
 
-# Verifica inputs do jogador e atribui os devidos valores ao atributo velocity
+# Verifica inputs do jogador e atribui os devidos valores ao atributo velocity na horizontal
 func horizontal_movement_env() -> void:
 	
 	# Captura os inputs
@@ -41,15 +44,22 @@ func horizontal_movement_env() -> void:
 	# pela velocidade configurada
 	velocity.x = input_direction * speed
 
+# Verifica inputs do jogador e atribui os devidos valores ao atributo velocity na vertical
 func vertical_movement_env() -> void:
+	# Se o personagem estiver no chão, a quantidade de pulos disponivel reseta
 	if is_on_floor():
 		jump_count = 0
 	
+	# Ao apertar espaço e se ainda houver pulos disponiveis...
 	if Input.is_action_just_pressed("jump") and jump_count < MAX_JUMP_COUNT:
+		# Incrementa a quantidade de pulos efetuados desde a ultima vez que o personagem
+		# colidiu com o chão
 		jump_count += 1
+		# Incrementa a velocidade de pulo
 		velocity.y = jump_speed
 
 
+# Impõe gravidade ao personagem
 func gravity(delta: float) -> void:
 	velocity.y += delta * player_gravity
 	if velocity.y >= player_gravity:
